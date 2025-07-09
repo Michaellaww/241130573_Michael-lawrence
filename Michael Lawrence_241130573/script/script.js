d09293bcecfefa4a95648eaab2391e56e1f54c4d
Michael Lawrence <michaelawrence31398@gmail.com>
2025-07-09 19:18:44 +0700

    menambahkan js1

--- Michael Lawrence_241130573/script/script.js
+++ Michael Lawrence_241130573/script/script.js
@@ -1,4 +1,14 @@
 document.querySelectorAll('.card-produk').forEach(card => {
     const input = card.querySelector('.quantity input');
-    const munus = card.querySelector('.quantity input');
+    const munus = card.querySelector('.minus');
+    const plus = card.querySelector('.plus');
+
+    plus.addEventListener('click', () => {
+        const harga = card.querySelector('.price');
+        const nominalharga = harga.textContent.match(/yd+/)[0];
+
+        let value = parseInt(input.value);
+        input.value = value + 1;
+        harga.innerHTML = '$${(nominalharga / value)'
+    })
 } )
